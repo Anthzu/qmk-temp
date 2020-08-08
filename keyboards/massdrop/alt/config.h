a76f8704dacb60c73b9daabf763732252fe017b9
Anthzu <69393237+Anthzu@users.noreply.github.com>
2020-08-08 10:20:26 -0500

    Update config.h

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -120,6 +120,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+/* Bootmagic Lite key configuration */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_LED_PROCESS_LIMIT 15
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
